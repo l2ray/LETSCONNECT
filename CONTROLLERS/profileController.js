2a1cb7e2e326f88079a8accf76c5b7c50bfb590d
l2ray <seguylinux@gmail.com>
2021-12-31 00:38:48 +0100

    A Commit for profileController ProfileRoute'

--- CONTROLLERS/profileController.js
+++ CONTROLLERS/profileController.js
@@ -1,5 +1,6 @@
 const ProfileSchema = require('../MODELS/Profile.model');
 const userSchema = require('../MODELS/user.model');
+const {validationResult} = require('express-validator/check')
 const profileList = (req,res)=>{
     return res.status(200).send("Returinig all profiles in the System...")
 }
@@ -15,7 +16,62 @@ const myProfile = async (req,res)=>{
         console.log("Sorry...Server Error Ooccured...");
     }
 }
+
+// @desc    Adding / Updating a Profile
+// @access  PUBLIC 
+// @route   POST api/profile/
+const createProfile = async(req,res)=>{
+    const errors = validationResult(req);
+    if(errors.errors.length >0){
+        return res.status(400).json({"Status":"Error","msg":errors.errors})
+    }
+    const {company,website,location,bio,status,githubusername,skills,youtube,facebook,twitter,instagram,linked} = req.body;
+    const profileObj = {};
+    if(company){profileObj.company = company}
+    if(website){profileObj.website = website}
+    if(location){profileObj.location = location}
+    if(bio) {profileObj.bio = bio}
+    profileObj.status = status;
+    if(githubusername){
+        profileObj.githubusername = githubusername;
+    }
+    profileObj.social = {};
+    if(youtube){profileObj.social.youtube = youtube}
+    if(facebook){profileObj.facebook = facebook}
+    if(twitter){profileObj.twitter=twitter}
+    if(instagram){profileObj.instagram = instagram}
+    if(linked){profileObj.linked=linked}
+    const skillsArr = [];
+    skills.split(",").map(s=>{
+        skillsArr.push(s.trim())
+    })
+    profileObj.skills =skillsArr;
+    profileObj.user = req.user.id;
+    console.log(req.user)
+    try {
+        const userFlag =await  ProfileSchema.findOne({user:req.user.id});
+        console.log(userFlag);
+        if(userFlag ){
+            console.log("Updating a Profile...")
+            const updateProfile = await ProfileSchema.findOneAndUpdate({user:req.user.id},{$set:profileObj})
+            await updateProfile.save();
+        }
+        else{
+            // update 
+            console.log("Creating a new Profile...")
+            const newProfile = new ProfileSchema(profileObj);
+            await newProfile.save();
+        }
+        return res.status(200).json({"Status":"Success","msg":profileObj});
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({"Status":"Error","msg":"Sorry There exists an error within the server..."})
+    }
+    
+
+}
 module.exports = {
     profileList :profileList,
-    myProfile:myProfile
+    myProfile:myProfile,
+    createProfile :createProfile
 }
--- ROUTES/profileRoute.js
+++ ROUTES/profileRoute.js
@@ -2,10 +2,15 @@ const express = require('express');
 const profileController = require('../CONTROLLERS/profileController');
 const Router = express.Router();
 const auth = require('../MIDWARE/auth.midware')
+const {check}= require('express-validator/check')
 
 
 Router.get("/",profileController.profileList);
 Router.get("/me",auth,profileController.myProfile);
+Router.post("/",[[
+    check("status","Status is Required").not().isEmpty(),
+    check("skills","Skills are required").not().isEmpty()
+],auth],profileController.createProfile);
 module.exports = {
     Router:Router
 };
