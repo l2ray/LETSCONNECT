d964e9ac48cb418446816fa067e6a513a0649f68
l2ray <seguylinux@gmail.com>
2022-01-04 03:36:23 +0100

    A Commit reflecting Post Model, Post Route and Post Controller

--- ROUTES/postRoute.js
+++ ROUTES/postRoute.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const postController = require('../CONTROLLERS/postController');
 const Router = express.Router();
+const {check} = require('express-validator/check');
+const auth = require('../MIDWARE/auth.midware');
 
 Router.get("/posts",postController.posts);
-
+Router.post("/add",[auth,[check("title","Sorry Title is required...").not().isEmpty()]],postController.addPost);
 module.exports = Router;
