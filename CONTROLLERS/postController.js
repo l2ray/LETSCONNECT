e8996b3112ddcfde93480b7a1d3a1a7673482a42
l2ray <seguylinux@gmail.com>
2022-01-04 03:38:25 +0100

    A Commit reflecting Post Model, Post Route and Post Controller

--- CONTROLLERS/postController.js
+++ CONTROLLERS/postController.js
@@ -1,8 +1,25 @@
+const PostsSchema = require('../MODELS/Post.model');
+const userShema = require('../MODELS/user.model');
+const profileSchema = require('../MODELS/Profile.model');
+const {validationResult} = require('express-validator/check');
 const posts = (req,res)=>{
     console.log("Returning all posts in the System...");
     res.status(200).send("Post Returning...")
 };
+const addPost = async(req,res)=>{
+    try {
+        const errors = validationResult(req);
+        console.log(errors.errors);
+        if(errors.errors.length > 0){
+            console.log(errors.errors);
+
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+}
 
 module.exports = {
-    posts:posts
+    posts,
+    addPost
 }
---
+++ MODELS/Post.model.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const postSchema = new mongoose.Schema({
+    user:{
+        type:mongoose.Types.ObjectId,
+        ref: 'users'
+    },
+    text:{
+        type:String,
+        required:true,
+
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    avatar:{
+        type:String,
+    },
+    like:[
+        {
+            user:{
+                type:mongoose.Types.ObjectId,
+                ref:'users'
+            }
+        }
+    ],
+    comments:[
+        {
+            user:{
+                type:mongoose.Types.ObjectId,
+                ref:'users'
+            },
+            text:{
+                type:String,
+                require:true
+            },
+            name:{
+                type:String
+            },
+            avatar:{
+                type:String
+            },
+            date:{
+                type:Date,
+                default:Date.now
+            }
+        }
+    ],
+    date:{
+        type:Date,
+        default:Date.now
+    }
+});
+
+module.exports = Post = mongoose.model("posts",postSchema);
