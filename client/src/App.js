8f28fc2c6e1739314e17b0ae76ad9d99370f47c3
l2ray <seguylinux@gmail.com>
2022-01-11 19:12:33 +0100

    A Commit including route setup Navbar Landing Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import React,{ Component } from 'react';
+import React,{ Component, Fragment } from 'react';
+import './App.css'
+import Landing from './Components/Layouts/Landing';
+import Navbar from './Components/Layouts/Navbar';
+import {BrowserRouter as Router,Routes, Route,Switch} from 'react-router-dom'
+import { Login } from './Components/auth/Login';
+import { Register } from './Components/auth/Register';
+const App = ()=>{
+  return(
+    <Router>
+    <Navbar />
+      <Routes>
+      <Fragment>
+      <Route exact path="/" element={<Landing/>} />
+      
+     
+  </Fragment>
+      </Routes>
+      <section className="container">
+        <Routes>
+          <Route exact path = "register" element={<Register />} />
+          <Route exact path="login" element={<Login />} />
+        </Routes>
+      </section>
+    </Router>
+  )
+}
+export default App;
 
+/*
 class App extends Component{
   constructor(){
     super()
@@ -14,4 +40,4 @@ class App extends Component{
     )
   }
 }
-export default App;
+*/
