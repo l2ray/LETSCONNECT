0c9ebfee6d792a042550405ad4efea946313f749
l2ray <seguylinux@gmail.com>
2022-01-11 17:34:14 +0100

    A Commit to include the Client Side REact Framework

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React,{ Component } from 'react';
+
+class App extends Component{
+  constructor(){
+    super()
+  }
+  render(){
+    return(
+      <div>
+        <h1>sdfsd</h1>
+        </div>
+    )
+  }
+}
+export default App;
